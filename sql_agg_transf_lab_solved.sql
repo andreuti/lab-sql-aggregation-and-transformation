eda512a2537aee9870386b5740592888eab279af
Andrés <andresmu97@outlook.es>
2025-05-17 08:29:31 +0200

    lab solved del todo

--- sql_agg_transf_lab_solved.sql
+++ sql_agg_transf_lab_solved.sql
@@ -28,3 +28,43 @@ SELECT
     END AS day_type
 FROM rental
 LIMIT 20;
+SELECT
+    title,
+    IFNULL(length, 'Not Available') AS rental_duration
+FROM film
+ORDER BY title ASC;
+SELECT
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    SUBSTRING(email, 1, 3) AS email_prefix
+FROM customer
+ORDER BY last_name ASC;
+-- Challenge 2
+SELECT COUNT(*) AS total_films
+FROM film;
+SELECT
+    rating,
+    COUNT(*) AS film_count
+FROM film
+GROUP BY rating;
+SELECT
+    rating,
+    COUNT(*) AS film_count
+FROM film
+GROUP BY rating
+ORDER BY film_count DESC;
+SELECT
+    rating,
+    ROUND(AVG(length), 2) AS avg_duration
+FROM film
+GROUP BY rating
+ORDER BY avg_duration DESC;
+SELECT
+    rating,
+    ROUND(AVG(length), 2) AS avg_duration
+FROM film
+GROUP BY rating
+HAVING avg_duration > 120;
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(*) = 1;
